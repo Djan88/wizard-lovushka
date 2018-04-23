1d6af7a0124a2c5be04584f87685c8993762aef8
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-23 17:28:05 +0300

    kassa-48

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -54,8 +54,8 @@
           </div>
         </div>
         <?php if(!is_user_logged_in()){ ?>
-          <button class="btn btn-primary ru_block" data-toggle="login" data-target="#login">Вход</button>
-          <button class="btn btn-primary en_block" data-toggle="login" data-target="#login">Enter</button>
+          <button class="btn btn-warning btn_login ru_block" data-toggle="modal" data-target="#login">Вход</button>
+          <button class="btn btn-warning btn_login en_block" data-toggle="modal" data-target="#login">Enter</button>
         <?php } ?>
       </div>
     </div>

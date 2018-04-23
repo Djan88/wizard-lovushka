2e37c348d04eaf89f4e23354b1c821b946c005d7
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-23 17:38:34 +0300

    kassa-51

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -513,7 +513,7 @@
     </div>
   </div>
   <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog modal-sm login_modal">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>

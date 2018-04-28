6769f69cee18e009283c4fe615d29852ad7050d9
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-29 00:05:09 +0300

    cabinet-5

--- wp-content/themes/lovushka/functions.php
+++ wp-content/themes/lovushka/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+add_filter("login_redirect", "sp_login_redirect", 10, 3);
+
+function sp_login_redirect($redirect_to, $request, $user){
+    if(is_array($user->roles))
+        if(in_array('administrator', $user->roles))
+            return home_url('/wp-admin/');
+    return home_url();
+}
 
 /* Отключаем админ панель для всех, кроме администраторов. */
 if (!current_user_can('administrator')):

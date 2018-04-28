1dc33e1f6554e3070642b0b89aeead76f3351613
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-28 13:53:59 +0300

    cabinet

--- wp-content/themes/lovushka/functions.php
+++ wp-content/themes/lovushka/functions.php
@@ -1,12 +1,5 @@
 <?php
-function users_redirect(){
-wp_redirect(site_url('/'));
-die();
-}
-if(!current_user_can('manage_options')){
-add_action('admin_init','users_redirect');
-add_filter('login_redirect', 'users_redirect');
-}
+
 
 /* Отключаем админ панель для всех, кроме администраторов. */
 if (!current_user_can('administrator')):

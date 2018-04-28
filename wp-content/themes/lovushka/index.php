1810bb716cd608b596df510fcf870522399dce93
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-28 16:37:42 +0300

    cabinet-2

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -340,8 +340,11 @@
         </div>
       <?php } ?>
     <?php } else { ?>
-      <h2><?php the_title()?></h2>
-      <p><h2><?php the_content()?></h2></p>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h2><?php the_title()?></h2>
+        <p><?php the_content()?></p>
+      <?php endwhile; ?>
+      <?php endif; ?>
     <?php } ?>
   </section><!-- end of doctor section -->
   <footer class="footer clearfix">

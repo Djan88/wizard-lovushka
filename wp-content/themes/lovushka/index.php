50bd3c958592c932d3af7c66ee1473ca2c7cff2b
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-28 16:42:32 +0300

    cabinet-3

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -341,8 +341,16 @@
       <?php } ?>
     <?php } else { ?>
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h2><?php the_title()?></h2>
-        <p><?php the_content()?></p>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h2><?php the_title()?></h2>
+            </div>
+            <div class="col-md-12">
+              <p><?php the_content()?></p>
+            </div>
+          </div>
+        </div>
       <?php endwhile; ?>
       <?php endif; ?>
     <?php } ?>

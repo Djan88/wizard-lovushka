eb94753a183234b192ddd9a751406c70de124cad
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-05-11 20:11:10 +0300

    status-3

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -193,7 +193,8 @@
                     <p class="en_block hidden">
                         Upon completion of payment, a confirmation letter will be sent to your mail. From now on, your access to the "Wizard Lovushka" program is active! Pleasant use!
                     </p>
-                    <h3></h3>
+                    <h4 class="ru_block">С любым возникшим у Вас вопросом с удовольствием поможет наша служба технической поддержки. Напишите на <a href="mailto:info@chikurov.com">info@chikurov.com</a>. Оперативно отвечаем 7 дней в неделю. По телефону консультаций не проводим!</h4>
+                    <h4 class="en_block hidden">With any question you have, our technical support will be pleased to help. Write on<a href="mailto:info@chikurov.com">info@chikurov.com</a>. We respond promptly 7 days a week. We do not hold consultations on the phone!</h4>
                   </div>
                   <h4 class="ru_block">Юридическая информация:</h4>
                   <h4 class="en_block hidden">Legal information:</h4>

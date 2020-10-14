fb857ccba8fbf7698ef747e158e0137800b9f184
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-10-14 11:20:26 +0300

    reverse-11

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -682,6 +682,13 @@
           <h4 class="modal-title" id="myModalLabel"><span class="ru_block">Реверсивное Центрирование</span><span class="hidden en_block">The Reverse Centering</span></h4>
         </div>
         <div class="modal-text">
+          <div class="reverce_age">
+            <table>
+              <tr>
+                <td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+              </tr>
+            </table>
+          </div>
           <div id="slider">
             <div id="custom-handle" class="ui-slider-handle"></div>
           </div>

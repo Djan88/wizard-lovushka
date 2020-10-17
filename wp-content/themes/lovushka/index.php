7a1601447ab2f4009bddcc49a2ab2d89a00e31a0
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-10-17 14:55:05 +0300

    lovushka-35

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -419,7 +419,7 @@
                       <button type="button" class="btn btn-default revece_bc" data-toggle="modal" data-target="#revece_bc">Регрессивное центрирование</button>
                     </div>
                   </div>
-                  <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12 en_block">
+                  <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12 hidden en_block">
                     <div class="btn-group revece_mode">
                       <button type="button" class="btn btn-default revece_bc" data-toggle="modal" data-target="#revece_bc">The Regress Centering</button>
                     </div>
@@ -430,7 +430,7 @@
                       <button type="button" class="btn btn-default" data-toggle="popover" data-placement="top" data-original-title="Регрессивное центрирование" data-content='Регрессивное центрирование доступно для пользователей расширенной лицензии'>Регрессивное центрирование</button>
                     </div>
                   </div>
-                  <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12 en_block">
+                  <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12 hidden en_block">
                     <div class="btn-group revece_mode">
                       <button type="button" class="btn btn-default" data-toggle="popover" data-placement="top" data-original-title="The Regress Centering" data-content='The Regress Centering is available for extended license users'>The Regress Centering</button>
                     </div>
@@ -715,7 +715,7 @@
         </div>
         <div class="modal-footer">
           <button class="btn btn-success ru_block reverce_acept pull-left" data-dismiss="modal" style="display: none;">Применить</button>
-          <button class="btn btn-success en_block reverce_acept pull-left" data-dismiss="modal" style="display: none;">Acept</button>
+          <button class="btn btn-success en_block hidden reverce_acept pull-left" data-dismiss="modal" style="display: none;">Acept</button>
           <button class="btn btn-warning reverce_clean_graph pull-left" style="display: none;"><i class="fa fa-trash"></i></button>
           <button class="btn btn-primary ru_block" data-dismiss="modal">Закрыть</button>
           <button class="btn btn-primary hidden en_block" data-dismiss="modal">Close</button>

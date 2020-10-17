20dac3e6344a779ce7d6dc924fdbc971618bcdb6
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-10-17 11:42:19 +0300

    lovushka-24

--- wp-content/themes/lovushka/js/script.js
+++ wp-content/themes/lovushka/js/script.js
@@ -499,7 +499,7 @@ jQuery('.instruction_block').on('click', function(event) {
         jQuery('.reverce_graph').append('<div class='+knife_rate_class+'></div>');
         jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
             left: +knife+20+'px',
-            height: knifeDateDiff*2+'px'
+            height: knifeDateDiff+'px'
         });
         knifeDateOld = knifeDate;
       },
